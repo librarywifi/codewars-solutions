acc5eef2cc522249329925ff85a4d6987afb61ce
kyletejuco <kgtejuco@gmail.com>
2022-05-13 01:35:38 -0700

    add solution004.js

---
+++ js/solution004.js
@@ -0,0 +1,16 @@
+// Parse nice int from char problem (5/13/22)
+
+function getAge(inputString) {
+  // return the girl's correct age as an integer. Happy coding :)
+  return Number(inputString.charAt(0));
+}
+
+// PREP - parameters, return, examples, pseudocode
+
+// parameter - string, first character is always a number
+
+// return - number, age of girl
+
+// example - getAge("3 years old"); returns 3
+
+// pseudocode - A string is entered, take the first character of the string and return it as an integer
