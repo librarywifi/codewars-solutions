c952e4c35a0ba4ec21e36a9d0c14def958d1df78
kgtejuco@gmail.com <kgtejuco@gmail.com>
2022-05-17 23:29:00 -0700

    add solution008.js

---
+++ js/solution008.js
@@ -0,0 +1,14 @@
+// Keep up the hoop
+
+function hoopCount(n) {
+  // p: integer, number of times the hoop goes round
+  // r: string, an encouraging message
+  // e: hoopCount(10) returns "Great, now move on to tricks"
+  // p: if, else statement returning a string based on number of hoops
+
+  if (n >= 10) {
+    return "Great, now move on to tricks";
+  } else {
+    return "Keep at it until you get it";
+  }
+}
