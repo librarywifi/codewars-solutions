1ea25536c644971d51f5ade9f5f61bd2f90c7f96
kgtejuco@gmail.com <kgtejuco@gmail.com>
2022-05-22 10:40:15 -0700

    update solution013.js

---
+++ js/solution013.js
@@ -0,0 +1,13 @@
+// Volume of a Cuboid
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    // P: three integers
+    // R: integer
+    // E: getVolumeOfCuboid(2, 2, 2) returns 8
+    // P:
+    // returns l * w * h
+
+    return length * width * height;
+  }
+}
