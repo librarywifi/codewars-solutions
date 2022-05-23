00062f2640d3bd73807d2038fac9f7d4155ffcf3
kgtejuco@gmail.com <kgtejuco@gmail.com>
2022-05-23 09:56:30 -0700

    add solution014.js

---
+++ js/solution014.js
@@ -0,0 +1,10 @@
+// Function 1 - hello world
+
+// P: no parameters
+// R: String, greeting
+// E: hello() returns "hello world!"
+// P: write a function, return a greeting when it is called
+
+function greet() {
+  return "hello world!";
+}
